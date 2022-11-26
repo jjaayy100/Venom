2f74ecb6c0cfe88f288b00c53b88aac5bd3034cc
Darien Ware <thedarienware@gmail.com>
2022-11-25 20:02:06 -0800

    updated header and snake.cpp

--- dware.h
+++ dware.h
@@ -5,3 +5,7 @@ extern void show_startup(int x, int y);
 unsigned int check_map(unsigned int k);
 
 void resize_map(int x, int y, int boarddim, int griddim);
+
+void dget_textures(GLuint BackggroundTexture);
+
+void get_snake_pos(int length, int snake_pos[][2]);
